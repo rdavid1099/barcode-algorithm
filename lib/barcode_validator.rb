2bf0a7fbaf841749fde7492f2d0e706c0cb355d1
Ryan Workman <rdavid1099@gmail.com>
2018-05-06 16:09:28 -0600

    Add verification functionality

--- lib/barcode_validator.rb
+++ lib/barcode_validator.rb
@@ -2,6 +2,40 @@ class BarcodeValidator
   attr_reader :type
 
   def initialize(type = 'ean')
-    @type = type.downcase
+    @type = if TYPES.include?(type.downcase)
+      type.downcase
+    else
+      error(ArgumentError, 'Invalid barcode type')
+    end
+  end
+  
+  def verify(val)
+    sanitized = sanitize(val).reverse
+    check_digit = sanitized.pop
+    (10 - (calculate_values(sanitized) % 10)) == check_digit.to_i
+  end
+  
+  private
+  TYPES = [
+    'upc',
+    'ean',
+    'itf'
+  ]
+  
+  def calculate_values(sanitized)
+    counter = sanitized.length + 1
+    sanitized.reduce(0) do |result, digit|
+      result + (digit.to_i * ((counter -= 1).even? ? 1 : 3))
+    end
+  end
+  
+  def sanitize(val)
+    val.chars.map do |char|
+      char if /[0-9]/.match(char)
+    end.compact
+  end
+  
+  def error(type, msg)
+    raise type, msg
   end
 end
--- test/barcode_validator_test.rb
+++ test/barcode_validator_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require './lib/barcode_validator'
+require 'pry'
 
 class BarcodeValidatorTest < Minitest::Test
   def test_initializes_with_default_type
@@ -12,5 +13,21 @@ class BarcodeValidatorTest < Minitest::Test
     bv = BarcodeValidator.new('itf')
 
     assert_equal 'itf', bv.type
+  end
+
+  def test_error_thrown_if_invalid_type_is_given
+    assert_raises(ArgumentError) { BarcodeValidator.new('fake') }
+  end
+  
+  def test_it_verifies_ean_barcode_number
+    bv = BarcodeValidator.new
+
+    assert_equal true, bv.verify('7987654321794')
+  end
+  
+  def test_it_verifies_with_dashes
+    bv = BarcodeValidator.new
+
+    assert_equal true, bv.verify('4-912345-123459')
   end
 end
